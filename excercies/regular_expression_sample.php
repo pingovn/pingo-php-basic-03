40cbcd1c6de0c597c2faa19e2ff994292f9d3bb1
vtthien <thienvt151@gmail.com>
2015-10-10 19:03:29 +0700

    Solution regular expression

---
+++ excercies/regular_expression_sample.php
@@ -0,0 +1,39 @@
+<?php 
+	//$string = file_get_contents("example.txt"); // Load text file contents
+	$string = "hom nay hoc bai ve regular expression do thienvu@gmail.com huong dan tai pingo@edu.vn";
+	// don't need to preassign $matches, it's created dynamically
+
+	// this regex handles more email address formats like a+b@google.com.sg, and the i makes it case insensitive
+	$pattern = '/[a-z0-9_\-\+]+@[a-z0-9\-]+\.([a-z]{2,3})(?:\.[a-z]{2})?/i';
+
+	// preg_match_all returns an associative array
+	preg_match_all($pattern, $string, $matches);
+
+	// the data you want is in $matches[0], dump it with var_export() to see it
+	var_dump($matches);
+	echo "<br/>";
+	var_export($matches[0]);
+	echo "<br/>";
+	var_dump($matches[0][0]);
+	echo "<br/>";
+	var_dump($matches[0][1]);
+
+?>
+
+<html>
+	<head>
+		<title>PINGO - Solution excercise 1</title>
+		<meta charset="UTF-8">		
+	</head>
+	<body>
+		<div align="center"><h1>TITLE: SOLUTION FOR REGULAR EXPRESSION</h1></div>
+		<div align="center">
+			
+		</div>
+		<div class="separate-line" align="center" style="border-top: solid 1px #111223;">			
+		</div>
+		<div class="solution-mesage" align="center">
+			
+		</div>		
+	</body>
+</html>
---
+++ excercies/solution_Regex_Bai_1.php
@@ -0,0 +1,55 @@
+<?php
+$isValid;
+$checkingState;
+$pattern = '([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})';
+$phone_card = (isset($_POST['phone_card']) ? $_POST['phone_card'] : null);
+var_dump($phone_card);
+if(isset($phone_card))
+{	
+	if (preg_match($pattern, $phone_card)) {
+    $isValid = true;
+	$checkingState = 2;
+} else {
+    $isValid = false;
+	$checkingState = 1;
+}
+}
+else
+{
+	$checkingState = 0;
+	$isValid = false;
+}
+
+?>
+<html>
+	<head>		
+	</head>
+	<body>
+		<form action="1.php" method="post">
+			Phone card: <input type="text" name="phone_card" id="phone_card">			
+			<br>
+			<?php
+				if (!$isValid) {
+					echo "<h1> Vui long nhap lai du lieu!</h1>";
+				}
+				else
+				{
+					echo "<h1> OK Du lieu dung...</h1>";
+				}
+				if ($checkingState == 0) {
+					echo "<h1> Vui long nhap du lieu!</h1>";
+				}
+				else if ($checkingState == 1)
+				{
+					echo "<h1> Du lieu khong hop le</h1>";
+				}
+				else
+				{
+					echo "<h1> OK Du lieu dung...</h1>";
+				}
+			?>
+			<br>
+			<input type="submit" value="submit">
+		</form>
+	</body>
+</html>
